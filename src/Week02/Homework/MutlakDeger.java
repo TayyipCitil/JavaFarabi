e9775e64628a0c91e2efa85da44d2be0ca09c2f7
Tayyip <tayyipcitil1@gmail.com>
2026-01-11 10:54:19 +0300

    update

---
+++ src/Week02/Homework/MutlakDeger.java
@@ -0,0 +1,11 @@
+package Week02.Homework;
+
+public class MutlakDeger {
+    static void main() {
+        int sayi=-21;
+        if(sayi<0) {
+            sayi*=-1;
+        }
+        System.out.println(sayi);
+    }
+}
