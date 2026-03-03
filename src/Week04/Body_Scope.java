264c17e95f9e2c772f451f7ee1c99c5b1e72a1be
Tayyip <tayyipcitil1@gmail.com>
2026-03-03 08:01:46 +0300

    update

--- src/Week04/Body_Scope.java
+++ src/Week04/Body_Scope.java
@@ -1,17 +1,19 @@
 package Week04;
 
 public class Body_Scope {//tekrar ettik
-    public void dunya(){
-        int altin=5;
-        if(true){//Türkiye
-            int tl=5;
-            altin=7;
-            if(true){//Sivas
-                int hamsi=56;
-                altin=8;
-                tl=3;
-                if(true){//merkez
-                    hamsi=45;
+    boolean deger = true;
+
+    public void dunya() {
+        int altin = 5;
+        if (deger) {//Türkiye
+            int tl = 5;
+            altin = 7;
+            if (deger) {//Sivas
+                int hamsi = 56;
+                altin = 8;
+                tl = 3;
+                if (deger) {//merkez
+                    hamsi = 45;
                 }
             }
         }
